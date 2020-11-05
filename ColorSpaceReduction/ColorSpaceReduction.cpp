7dc07a464091c37225a80fca4d099c664b9f69ee
neil <nh4096@hotmail.com>
2020-11-05 16:38:19 +0800

    update Mat01

---
+++ ColorSpaceReduction/ColorSpaceReduction.cpp
@@ -0,0 +1,37 @@
+﻿#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    const string cmdkeys =
+        "{help h ? |        | print this message       }"
+        "{divide d | 4      | divide color space with d}"
+        "{@image   | <none> | input image              }";
+    CommandLineParser parser(argc, argv, cmdkeys);
+    parser.about("Color space reduction tool");
+
+	if (parser.has("help")) {
+		parser.printMessage();
+		return EXIT_SUCCESS;
+	}
+	if (!parser.check()) {
+		parser.printErrors();
+		return EXIT_FAILURE;
+	}
+
+	// check divide value
+	int divide = parser.get<int>("divide"); 
+	
+	// read image
+	string imageFile = parser.get<string>(0); 
+	Mat image = imread(imageFile, IMREAD_COLOR);
+	if (image.empty()) {
+		cout << "Cannot read from file: " << imageFile << endl;
+		return EXIT_FAILURE;
+	}
+
+}
--- Mat01/Mat01.cpp
+++ Mat01/Mat01.cpp
@@ -4,9 +4,9 @@
 using namespace cv;
 using namespace std;
 
+// multiple Mat objects share underlying matrix
 void mat_example_1()
-{
-    // multiple Mat object share underlying matrix
+{    
     Mat a(3, 3, CV_8UC1, 128);
     Mat b(a);
     Mat c = a;
@@ -18,17 +18,50 @@ void mat_example_1()
     cout << a << endl;
 }
 
+// create ROI
 void mat_example_2()
 {
-    // create ROI
     Mat a(3, 3, CV_8UC1, 128);
     Mat a_roi(a, Rect(1, 1, 2, 2));
     a_roi = 20;
     cout << a << endl;
 }
 
+// create Mat explicitly
+void mat_example_3()
+{    
+    Mat a(3, 3, CV_16UC3, Scalar(0, 255, 192, 128));
+    cout << a << endl;
+
+    Mat b(2, 2, CV_8SC2, Scalar(-64, 64));
+    cout << b << endl;
+
+    //Mat c(3, {2,3,4 }, CV_16FC1, Scalar::all(25.0));
+
+    // random values
+    Mat r(Size(3, 2), CV_8SC2);
+    randu(r, Scalar::all(-128), Scalar::all(127));
+    cout << r << endl;
+
+    // create via vector
+    vector<unsigned char> v;
+    for (int i = 0; i < 3; ++i) {
+        v.push_back(i * 20);
+    }
+    cout << Mat(v) << endl;
+    //Mat m(3, 1, CV_8UC1); m.at(0) = 0; m[1] = 20; m[2] = 40;
+    //cout << m << endl;
+
+    cout << Mat::zeros(2, 3, CV_32FC1) << endl; // (nrow,ncol)
+
+    cout << Mat::ones(Size(3, 4), CV_16FC2) << endl; // Size(width,height)
+
+    cout << Mat::eye(3, 4, CV_8UC1) << endl;
+}
+
 int main()
 {
     //mat_example_1();
-    mat_example_2();
+    //mat_example_2();
+    mat_example_3();
 }
