c4da747fa2e776af2c5c0f8dd8941ac5baefd0c3
neil <nh4096@hotmail.com>
2020-11-09 10:56:00 +0800

    add colorspacereduction

--- ColorSpaceReduction/ColorSpaceReduction.cpp
+++ ColorSpaceReduction/ColorSpaceReduction.cpp
@@ -5,6 +5,33 @@
 using namespace cv;
 using namespace std;
 
+// Use lookup table for faster mapping
+void compute_table(unsigned char table[], unsigned int d)
+{
+	for (int i = 0; i < 256; ++i) {
+		table[i] = i / d * d;
+	}
+}
+
+void apply_reduction(Mat& image, const unsigned char table[])
+{
+	switch (image.channels())
+	{
+	case 1:
+	{
+		MatIterator_<unsigned char> it;
+		for (it = image.begin<unsigned char>(); it != image.end<unsigned char>(); it++) {
+			*it = table[*it];
+		}
+		break;
+	}
+	case 3:
+	{
+		break;
+	}		
+	}
+}
+
 int main(int argc, char** argv)
 {
     const string cmdkeys =
@@ -25,7 +52,8 @@ int main(int argc, char** argv)
 
 	// check divide value
 	int divide = parser.get<int>("divide"); 
-	
+	assert(divide > 0 && divide < 128);
+
 	// read image
 	string imageFile = parser.get<string>(0); 
 	Mat image = imread(imageFile, IMREAD_COLOR);
@@ -33,5 +61,13 @@ int main(int argc, char** argv)
 		cout << "Cannot read from file: " << imageFile << endl;
 		return EXIT_FAILURE;
 	}
+	
+	unsigned char table[256];
+	compute_table(table,static_cast<unsigned char>(divide));
 
+	imshow("input", image); waitKey(0);
+
+	apply_reduction(image, table);
+
+	imshow("output", image); waitKey(0);
 }
